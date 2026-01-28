5feefa2955c4b04d5c873a70f642fdd0c6c4720a
Peehu Mishra <peehumishra1308@gmail.com>
2026-01-28 21:50:32 +0530

    updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useRef, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 import logo from "../assets/logoimg.svg";
-import Logo from "../assets/logoimg.svg?react";
+import Logo from "../assets/image.png";
+
 
 
 
@@ -342,12 +343,12 @@ const Navbar = () => {
     <div ref={navbarRef} className="md:fixed md:top-0 md:left-0 md:right-0 md:z-[1000] relative">
       <div className="flex items-center justify-between bg-[#1F2933] px-4 md:px-8 py-4 text-white">
         <div className="flex items-center gap-4 md:gap-10">
-          {/* <Link to="/">
-            <img src={logo} alt="logo" className="text-white h-12 md:h-16 w-12 md:w-16 object-contain" />
-          </Link> */}
           <Link to="/">
+            <img src={Logo} alt="logo" className="text-white h-24 md:h-16 w-12 md:w-16 object-contain" />
+          </Link>
+          {/* <Link to="/">
   <Logo className="h-12 w-12 text-white hover:text-[#F49426]" />
-</Link>
+</Link> */}
 
 
           <div className="hidden md:flex items-center gap-6">
